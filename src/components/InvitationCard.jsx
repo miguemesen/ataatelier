4434de0dd95cf7f8326f2576e6c3736e1ca0e6d7
miguemesen <miguemesen12@gmail.com>
2026-07-08 03:58:34 -0600

    Minor changes

--- src/components/InvitationCard.jsx
+++ src/components/InvitationCard.jsx
@@ -1,11 +1,12 @@
 export default function InvitationCard({ imageUrl, invitees }) {
   const spotsLabel = invitees === 1 ? 'espacio' : 'espacios'
+  const plural = invitees === 1 ? 'Le' : 'Les'
 
   return (
     <div className="invitation-card">
       <img src={imageUrl} alt="Invitación" />
       <div className="invitation-spots-overlay">
-        <span className="invitation-spots-line-1">Le hemos reservado</span>
+        <span className="invitation-spots-line-1">{plural} hemos reservado</span>
         <span className="invitation-spots-line-2">{invitees} {spotsLabel}.</span>
       </div>
     </div>
